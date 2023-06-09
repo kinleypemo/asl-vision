1296b533dd29aade5475e4f49a21885c908a4c24
12200058 <12200058.gcit@rub.edu.bt>
2023-06-09 20:15:26 +0600

    update

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ def video_feed():
     return Response(gen_frames(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = "0.0.0.0", port=5000 ,debug=True)
 
 
 # from flask import Flask, render_template, Response, redirect, url_for
